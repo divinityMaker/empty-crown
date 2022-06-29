e000f635324206b73900f26d8cd42892ff7bf246
divinityMaker <truedmaker@gmail.com>
2022-06-29 05:43:58 -0300

    better-console.log

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,13 @@ const App = () => {
   }, [])
 
   setTimeout(window.onload = () =>  {
+      console.log("%c ", "font-size: 45px;");
       console.log("%cYou know what I gotta say", "color: #ffff; background-color: #b71540; font-size: 45px; font-weight: bold");
       console.log("%cKill 'em all and get locked away", "color: #ffff; background-color: #b71540; font-size: 45px; font-weight: bold");
       console.log("%cKill yourself and just end this pain", "color: #ffff; background-color: #b71540; font-size: 45px; font-weight: bold");
       console.log("%cYou gonna burn in hell anyways", "color: #ffff; background-color: #b71540; font-size: 45px; font-weight: bold");
       console.log("%cFuck the preachers and what they say", "color: #ffff; background-color: #b71540; font-size: 45px; font-weight: bold");
+      console.log("%c ", "font-size: 45px;");
   }, 1000);
 
 
